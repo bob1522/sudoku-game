813fa9240b561e2aa000b923e33b39ff7ef4b9a8
bob1522 <114647018+bob1522@users.noreply.github.com>
2024-11-03 15:25:27 +1100

    Update sudoku-game.js

--- sudoku-game.js
+++ sudoku-game.js
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', function() {
     solveGame();
   });
   // Event listener for 'Print' button
-  document.getElementById('print-btn').addEventListener('click', () => {
+  document.getElementById('print-btn').addEventListener('click', function() {
     window.print();
   });
   
@@ -205,10 +205,7 @@ function fillPuzzle(solution) {
     cell.value = solution[index];
   });
 }
-// Event listener for 'Print' button
-  document.getElementById('print-btn').addEventListener('click', function() {
-    window.print();
-  });
+
 
                                                        
 
